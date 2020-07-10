22712ab54f2eb7c38ad09c77c19f0e705ddc8977
Jackson Reynolds <jwreynold@gmail.com>
2020-07-10 14:14:23 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,12 +1,20 @@
 class School
   # attr_accessor
-  attr_reader :school_name
+  attr_reader :school_name, :roster
 
-  ROSTER = []
+  ROSTER = {}
 
   def initialize(this_school_name)
     @school_name = this_school_name
   end #initialize
+
+  def add_student(student_name, grade)
+    if ROSTER[grade]
+      ROSTER[grade] << student_name
+    else
+      ROSTER[grade] = []
+      ROSTER[grade] << student_name
+    end #if
 
 
 end #School
