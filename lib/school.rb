89ea0acb2055ec20e8312ff15ce0263618f373ed
Jackson Reynolds <jwreynold@gmail.com>
2020-07-10 13:58:34 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,7 +1,12 @@
 class School
-  attr_accessor
+  # attr_accessor
   attr_reader :school_name
+
+  ROSTER = []
+  
   def initialize(this_school_name)
     @school_name = this_school_name
   end #initialize
+
+  
 end #School
